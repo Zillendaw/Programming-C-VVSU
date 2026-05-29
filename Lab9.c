6cbe087b71ef7fe1162ce66cd2476c9ffe97b344
Sanyek004 <sasha88543@gmail.com>
2026-05-29 22:48:30 +1000

    Update Lab9

--- Lab9.c
+++ Lab9.c
@@ -7,20 +7,29 @@ int main(int argc, char **argv)
 {
     char str[256] = "-123.5 + 4 - 456+56";
     
+    char formatted_str[512] = {0};
+    int j = 0;
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++)
+    {
+		if (str[i] == '+' || str[i] == '-')
+		{
+			formatted_str[j++] = ' ';
+			formatted_str[j++] = str[i];
+			formatted_str[j++] = ' ';
+		}
+		else
+		{
+			formatted_str[j++] = str[i];			
+		}
+	}
+    
     char delim[] = " ";
-    char symbol_plus = '+';
-    char symbol_minus = '-';
+    char *token = strtok(formatted_str,delim);
     
-    char *token = strtok(str,delim);
     double result = 0;
-    bool is_plus = 1;
-    
+    bool is_plus = 1; // 1 - Сложениеl ; 0 - Вычитание    
     while (token != NULL)
     {	
-		double num = strtod(token,NULL);
-		printf("result: %f\n",result);
-		printf("token: '%s'\n", token);
-		
 		if (strcmp(token,"+") == 0)
 		{
 			is_plus = 1;
@@ -29,22 +38,24 @@ int main(int argc, char **argv)
 		{
 			is_plus = 0;	
 		}
-		
-		if (is_plus)
-		{
-			result += num;
-		}
 		else
 		{
-			result -= num;
+			double num = strtod(token,NULL);
+			
+			if (is_plus)
+			{
+				result += num;
+			}
+			else
+			{
+				result -= num;
+			}
 		}
 		
 		token = strtok(NULL,delim);
-		if (token == NULL)
-		{
-			printf("%f\n",result);
-		}
 	}
+	
+	printf("Result: %f\n", result);
     	
 	return 0;
 }
