68940e5fceb01f0420eb4006a431152fcb45ce1b
Sanyek004 <sasha88543@gmail.com>
2026-06-19 17:16:42 +1000

    Lab14: Reverse function use setjmp

--- main14.c
+++ main14.c
@@ -2,23 +2,25 @@
 #include <setjmp.h>
 
 jmp_buf buffer;
-int final_sum = 0;
+long long final_sum = 0;
 
-int fibonacci(int n)
+void fibonacci(int target, int current_step,int a, int b)
 {
-	//printf("%d\n",n);
-	
-	if (n < 0)
+	if (target < 0)
 	{
 		printf("Число Фибоначчи не может быть меньше 0!\n");
-		longjmp(buffer,228);		
+		longjmp(buffer,2026);		
 	}
 	
-	if (n == 0) return 0;
-	if (n == 1) return 1;
+	final_sum += a;
 	
-	return fibonacci(n-1) + fibonacci(n-2);
-	longjmp(buffer,1);
+	if (current_step == target)
+	{
+		longjmp(buffer,1);
+	}
+
+	fibonacci(target, current_step + 1, b, (a + b));
+	
 }
 
 int main(int argc, char **argv)
@@ -28,12 +30,17 @@ int main(int argc, char **argv)
 	
 	if (error_code == 0)
 	{
-		printf("%d-е число Фиббоначи: %d; Код: %d\n", n, fibonacci(n),error_code);
+		fibonacci(n,1,1,1);
+	}
+	else if (error_code == 1)
+	{
+		printf("Сумма первых %d чисел Фибоначчи: %lld (Код: %d)\n", n, final_sum, error_code);
 	}
 	else
 	{
 		printf("Ошибка! Не получилось... Код ошибки: %d\n", error_code);
 	}
+	
 	return 0;
 }
 
